34a51dfbd1110b3c2f9ba75d0290460e6f7d24d9
Aleksandr Dulov <Aleksandr_Dulov@epam.com>
2020-09-19 14:58:45 +0300

    Finish

--- src/pages/album-info-page/albums/components/platform-links/platform-links.jsx
+++ src/pages/album-info-page/albums/components/platform-links/platform-links.jsx
@@ -20,126 +20,116 @@ const PlatformLinksComponent = ({
   amazonLink,
   anghamiLink,
   napsterLink,
-}) => {
-
-  const platformLinks = useMemo(() => document.getElementsByClassName('links__item'), []);
-    
-  setTimeout(() => {
-      for (const platform of platformLinks)
-        platform.classList.add('active')
-  }, 0)
-  
-  return (
-    <div className="links">
-      {vkLink !== undefined ? (
-        <div className="links__item">
-          <div className="links__name">
-            <img src={vk} alt="" />
-            <div>VK</div>
-          </div>
-          <a className="link__button" href={vkLink} target="_blank">
-            Открыть
-          </a>
+}) => (
+  <div className="links">
+    {vkLink !== undefined ? (
+      <div className="links__item">
+        <div className="links__name">
+          <img src={vk} alt="" />
+          <div>VK</div>
         </div>
-      ) : null}
+        <a className="link__button" href={vkLink} target="_blank">
+          Открыть
+        </a>
+      </div>
+    ) : null}
 
-      {yandexLink !== undefined ? (
-        <div className="links__item">
-          <div className="links__name">
-            <img src={yandex} alt="" />
-            <div>Yandex Music</div>
-          </div>
-          <a className="link__button" href={yandexLink} target="_blank">
-            Открыть
-          </a>
+    {yandexLink !== undefined ? (
+      <div className="links__item">
+        <div className="links__name">
+          <img src={yandex} alt="" />
+          <div>Yandex Music</div>
         </div>
-      ) : null}
+        <a className="link__button" href={yandexLink} target="_blank">
+          Открыть
+        </a>
+      </div>
+    ) : null}
 
-      {googleLink !== undefined ? (
-        <div className="links__item">
-          <div className="links__name">
-            <img src={google} alt="" />
-            <div>Google Play</div>
-          </div>
-          <a className="link__button" href={googleLink} target="_blank">
-            Открыть
-          </a>
+    {googleLink !== undefined ? (
+      <div className="links__item">
+        <div className="links__name">
+          <img src={google} alt="" />
+          <div>Google Play</div>
         </div>
-      ) : null}
+        <a className="link__button" href={googleLink} target="_blank">
+          Открыть
+        </a>
+      </div>
+    ) : null}
 
-      {youtubeLink !== undefined ? (
-        <div className="links__item">
-          <div className="links__name">
-            <img src={youtube} alt="" />
-            <div>YouTube Music</div>
-          </div>
-          <a className="link__button" href={youtubeLink} target="_blank">
-            Открыть
-          </a>
+    {youtubeLink !== undefined ? (
+      <div className="links__item">
+        <div className="links__name">
+          <img src={youtube} alt="" />
+          <div>YouTube Music</div>
         </div>
-      ) : null}
+        <a className="link__button" href={youtubeLink} target="_blank">
+          Открыть
+        </a>
+      </div>
+    ) : null}
 
-      {appleLink !== undefined ? (
-        <div className="links__item">
-          <div className="links__name">
-            <img src={apple} alt="" />
-            <div>Apple Music</div>
-          </div>
-          <a className="link__button" href={appleLink} target="_blank">
-            Открыть
-          </a>
+    {appleLink !== undefined ? (
+      <div className="links__item">
+        <div className="links__name">
+          <img src={apple} alt="" />
+          <div>Apple Music</div>
         </div>
-      ) : null}
+        <a className="link__button" href={appleLink} target="_blank">
+          Открыть
+        </a>
+      </div>
+    ) : null}
 
-      {spotifyLink !== undefined ? (
-        <div className="links__item">
-          <div className="links__name">
-            <img src={spotify} alt="" />
-            <div>Spotify</div>
-          </div>
-          <a className="link__button" href={spotifyLink} target="_blank">
-            Открыть
-          </a>
+    {spotifyLink !== undefined ? (
+      <div className="links__item">
+        <div className="links__name">
+          <img src={spotify} alt="" />
+          <div>Spotify</div>
         </div>
-      ) : null}
+        <a className="link__button" href={spotifyLink} target="_blank">
+          Открыть
+        </a>
+      </div>
+    ) : null}
 
-      {amazonLink !== undefined ? (
-        <div className="links__item">
-          <div className="links__name">
-            <img src={amazon} alt="" />
-            <div>Amazon Music</div>
-          </div>
-          <a className="link__button" href={amazonLink} target="_blank">
-            Открыть
-          </a>
+    {amazonLink !== undefined ? (
+      <div className="links__item">
+        <div className="links__name">
+          <img src={amazon} alt="" />
+          <div>Amazon Music</div>
         </div>
-      ) : null}
+        <a className="link__button" href={amazonLink} target="_blank">
+          Открыть
+        </a>
+      </div>
+    ) : null}
 
-      {anghamiLink !== undefined ? (
-        <div className="links__item">
-          <div className="links__name">
-            <img src={anghami} alt="" />
-            <div>Anghami</div>
-          </div>
-          <a className="link__button" href={anghamiLink} target="_blank">
-            Открыть
-          </a>
+    {anghamiLink !== undefined ? (
+      <div className="links__item">
+        <div className="links__name">
+          <img src={anghami} alt="" />
+          <div>Anghami</div>
         </div>
-      ) : null}
+        <a className="link__button" href={anghamiLink} target="_blank">
+          Открыть
+        </a>
+      </div>
+    ) : null}
 
-      {napsterLink !== undefined ? (
-        <div className="links__item">
-          <div className="links__name">
-            <img src={napster} alt="" />
-            <div>Napster</div>
-          </div>
-          <a className="link__button" href={napsterLink} target="_blank">
-            Открыть
-          </a>
+    {napsterLink !== undefined ? (
+      <div className="links__item">
+        <div className="links__name">
+          <img src={napster} alt="" />
+          <div>Napster</div>
         </div>
-      ) : null}
-    </div>
-  );
-};
+        <a className="link__button" href={napsterLink} target="_blank">
+          Открыть
+        </a>
+      </div>
+    ) : null}
+  </div>
+);
 
 export const PlatformLinks = memo(PlatformLinksComponent);
