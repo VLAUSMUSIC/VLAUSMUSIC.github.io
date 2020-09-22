4ffa192f25a636e8ae2aaeb3426244105a99feb1
Aleksandr Dulov <Aleksandr_Dulov@epam.com>
2020-09-22 22:49:17 +0300

    Styled 404 page and fixed error with albums-store

--- docs/10.5044f9d477c3cb3e48c0.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{35:function(n,w,o){}}]);
--- src/components/404/page-404.jsx
+++ src/components/404/page-404.jsx
@@ -1,3 +1,26 @@
 import React from 'react';
+import './page-404.css';
+import { useHistory } from 'react-router';
 
-export const Page404 = () => <div>404</div>;
+export const Page404 = () => {
+    const history = useHistory();
+    
+    const goHome = () => {
+        history.replace('/');
+    }
+
+    console.log(history);
+
+    return (
+        <div className="container_not_found">
+            <div className="not_found">
+                <div className="code">404</div>
+                <div className="code-text">
+                    Страница "<b>{history.location.pathname}</b>"  - не найдена! 
+                    Возможно, вы ввели неправильный адрес страницы или ваш браузер устарел.
+                </div>
+                <button className="go-home" onClick={goHome}>На главную</button>
+            </div>
+        </div>
+    );
+};
