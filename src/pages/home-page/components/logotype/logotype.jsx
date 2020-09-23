876337d2ae6c4d6fee848e15c3da9d2a53038403
Aleksandr Dulov <Aleksandr_Dulov@epam.com>
2020-09-23 22:54:21 +0300

    Added animation on home page

--- src/pages/home-page/components/logotype/logotype.jsx
+++ src/pages/home-page/components/logotype/logotype.jsx
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import './logotype.css'
 import tree from '../../../../assets/images/icons/tree.png'
 import play from '../../../../assets/images/icons/play.png'
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
-export const Logotype = () => (
-    <div className='logotype'>
-        <div className='logotype__vlaus'>
-            <div className='logotype__vlaus-text'>
-                VLAUS
+export const Logotype = () => {
+    const history = useHistory();
+
+    const { vlaus, music, link } = useMemo(() => ({
+        vlaus: document.getElementsByClassName('logotype__vlaus'),
+        music: document.getElementsByClassName('logotype__music'),
+        link: document.getElementsByClassName('link'),
+    }), [document]);
+
+    const goToAlbums = () => {
+        vlaus[0].classList.add('moved');
+        music[0].classList.add('moved');
+        link[0].classList.add('moved');
+        setTimeout(() => {
+            history.push('albums');
+        }, 1000)
+    };
+
+    return (
+        <div className='logotype' id="swup">
+            <div className='logotype__vlaus'>
+                <div className='logotype__vlaus-text'>
+                    VLAUS
+                </div>
+                <img className="tree" src={tree} alt="tree"/>
             </div>
-            <img className="tree" src={tree} alt="tree"/>
-        </div>
-        <Link className="link" to='/albums'>
-            <img className="play" src={play} alt="play"/>
-        </Link>
-        <div className='logotype__music'>
-            <div className='logotype__music-text'>
-                MUSIC
+            <div className="link" onClick={goToAlbums}>
+                <img className="play" src={play} alt="play"/>
             </div>
-        </div>
-    </div> 
-);
+            <div className='logotype__music'>
+                <div className='logotype__music-text'>
+                    MUSIC
+                </div>
+            </div>
+        </div> 
+    );
+}
