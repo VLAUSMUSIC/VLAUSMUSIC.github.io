2146b4e41430fd5220b855346407360db60e28d6
Aleksandr Dulov <du1ov@yandex.ru>
2021-03-06 00:28:59 +0300

    Added new album template

---
+++ src/pages/album/dusha-diktovala-a-ya-zapisyval/dusha-diktovala-a-ya-zapisyval.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import '@/pages/album/dusha-diktovala-a-ya-zapisyval/dusha-diktovala-a-ya-zapisyval.css';
+import { BackHomeButton } from '@@/back-home-button/back-home-button'
+
+import { PlatformLinks } from '@/pages/album/components/platform-links/platform-links';
+import { InfoBlock } from '@/pages/album/components/info-block/info-block';
+
+const DushaDiktovala = ({
+  singer,
+  trackName,
+  images,
+  platforms,
+}) => (
+  <div className="background-image">
+    <div className="container-block">
+      <BackHomeButton isWhite />
+      <InfoBlock
+        singer={singer}
+        trackName={trackName}
+        image={images.image}
+        metaImage={images.metaImage}
+      />
+      <PlatformLinks
+        vkLink={platforms.vkLink}
+        yandexLink={platforms.yandexLink}
+        googleLink={platforms.googleLink}
+        youtubeLink={platforms.youtubeLink}
+        appleLink={platforms.appleLink}
+        spotifyLink={platforms.spotifyLink}
+        amazonLink={platforms.amazonLink}
+        anghamiLink={platforms.anghamiLink}
+        napsterLink={platforms.napsterLink}
+      />
+    </div>
+  </div>
+);
+
+export default DushaDiktovala;
--- src/store/albums.js
+++ src/store/albums.js
@@ -1,6 +1,9 @@
 import kazhdomu_svoyo from '@/assets/images/albums-images/kazhdomu-svoyo/kazhdomu-svoyo.jpg';
 import notes from '@/assets/images/icons/music.png';
 
+import dusha_diktovala_a_ya_zapisyval from '@/assets/images/albums-images/dusha-diktovala-a-ya-zapisyval/dusha-diktovala-a-ya-zapisyval.jpg';
+import musical_notes from '@/assets/images/icons/musical-notes.png';
+
 const SINGER = 'VLAUS';
 
 export const ALBUMS = [
@@ -28,6 +31,26 @@ export const ALBUMS = [
       anghamiLink: 'https://play.anghami.com/song/81957561',
       napsterLink:
         'https://us.napster.com/artist/vlaus/album/--495387378/track/-',
+    },
+  },
+
+  {
+    id: 'dusha-diktovala-a-ya-zapisyval',
+    singer: SINGER,
+    trackName: 'Душа диктовала, а я записывал',
+    images: {
+      image: dusha_diktovala_a_ya_zapisyval,
+      metaImage: musical_notes,
+    },
+    platforms: {
+      vkLink: 'https://vk.com/music/album/-2000715295_10715295_f1485365ec05bfc38e',
+      yandexLink: 'https://music.yandex.ru/album/14400419',
+      youtubeLink: 'https://music.youtube.com/playlist?list=OLAK5uy_nkGFP_u9ZTozfTJe-CdcvmpniKHGJyMdA',
+      appleLink: 'https://music.apple.com/ru/album/%D0%B4%D1%83%D1%88%D0%B0-%D0%B4%D0%B8%D0%BA%D1%82%D0%BE%D0%B2%D0%B0%D0%BB%D0%B0-%D0%B0-%D1%8F-%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D1%8B%D0%B2%D0%B0%D0%BB/1558312977?ls',
+      spotifyLink: 'https://open.spotify.com/album/6h4FIwU6HMKNxplZlQrtrU?si=ZUlMnKAKQG2PJocIqB9n2A&nd=1',
+      amazonLink: 'https://music.amazon.com.au/albums/B08YX2S8TS?marketplaceId=A39IBJ37TRP1C6&musicTerritory=AU',
+      anghamiLink: 'https://play.anghami.com/album/1018662702',
+      napsterLink: 'https://us.napster.com/artist/vlaus/album/--565772689',
     },
   },
 ];
